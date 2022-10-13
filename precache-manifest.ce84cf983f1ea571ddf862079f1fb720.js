98fd28a7836af293cc1d14585bf23a6335b58737
tharanidharan25 <tharanitharan71@gmail.com>
2022-10-13 12:51:23 +0530

    Updates

---
+++ precache-manifest.ce84cf983f1ea571ddf862079f1fb720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5189404c344b45e2ea570ebcd406fbd8",
+    "url": "/react_tutorial_advanced/index.html"
+  },
+  {
+    "revision": "6000ac27b9a26e21070f",
+    "url": "/react_tutorial_advanced/static/css/main.52b06a7b.chunk.css"
+  },
+  {
+    "revision": "3ac27f6efea5dbf41542",
+    "url": "/react_tutorial_advanced/static/js/2.88bf9f60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_tutorial_advanced/static/js/2.88bf9f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6000ac27b9a26e21070f",
+    "url": "/react_tutorial_advanced/static/js/main.e12ada65.chunk.js"
+  },
+  {
+    "revision": "e83e89ab99ef42c8d864",
+    "url": "/react_tutorial_advanced/static/js/runtime-main.30b0718f.js"
+  }
+]);
