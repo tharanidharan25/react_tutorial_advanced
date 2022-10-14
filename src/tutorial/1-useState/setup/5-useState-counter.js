5dc24549bc0359150dad0ee828b751a72653a30a
tharanidharan25 <tharanitharan71@gmail.com>
2022-10-14 16:28:47 +0530

    Added 5-useState-counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,9 +2,22 @@ import React, { useState } from "react";
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+
   const reset = () => {
     setValue(0);
   };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      // WHEN WE USE FUNCTION INSIDE setValue, IT WILL USE THE PRESENT VALUE, ELSE IT WILL TAKE OLDER VALUE WHICH IN THIS CASE IS 0 EVEN WHEN WE CLICK THE BUTTON MORE THAN ONCE
+      // BUT WHEN WE USE FUNCTION, THE VALUE WILL INCREASE AS WE CLICK AND WILL GET UPDATED AFTER THE TIMER'S RUN OUT
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
@@ -18,6 +31,13 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase Later
         </button>
       </section>
     </>
